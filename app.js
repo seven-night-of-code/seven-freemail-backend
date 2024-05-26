7cd43b00b5a9fc1858d1506e6356232248d88185
Abdoul Idriss <Abdoulben44@gmail.com>
2024-05-26 06:12:06 +0100

    added the route to the app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const nodemailer = require("nodemailer");
 const apikeyroute = require("./routes/apikey.route")
+const { Login } = require("./controllers/users.controller");
 dotenv.config();
 
 console.log(process.env.MONGO_URL);
@@ -20,6 +21,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use("/api/key", apikeyroute);
+app.use(Login);
 app.listen( process.env.PORT, () => {
     console.log("app listening on :" + process.env.PORT)
 });
--- routes/user.route.js
+++ routes/user.route.js
@@ -5,6 +5,8 @@ const userController = require('../controllers/users.controller')
 
 router.post('/Login', userController.create);
 
+router.post('/')
+
 router.get('/', userController.create )
 
 module.exports = router;
